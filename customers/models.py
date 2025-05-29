7d6654a79733f4f4fca4d1c2fd0b8c9890d912b6
cossettealexis <gabuyacossettealexis@gmail.com>
2025-05-29 22:55:41 +0800

    Add Basic Models

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from core.models import Region  # Import Region from core
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    contact_number = models.CharField(max_length=20)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name='customers')
+
+    def __str__(self):
+        return self.name
